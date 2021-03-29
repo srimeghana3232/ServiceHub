a553c7b7609ee1cde1f9e810192926a77d600832
srimeghana3232 <srimeghana.guntur32@gmail.com>
2021-03-29 21:58:59 +0100

    electronicpage

---
+++ app/src/main/java/uk/ac/tees/w9336459/servicehub/electronic_tile.java
@@ -0,0 +1,14 @@
+package uk.ac.tees.w9336459.servicehub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class electronic_tile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_electronic_tile);
+    }
+}
